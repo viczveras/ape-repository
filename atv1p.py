ad81c7df2ec0a9f87f8ed7c463eb5fe362847fc9
Victor Véras <60857603+viczveras@users.noreply.github.com>
2024-10-31 17:51:55 -0300

    sequencial_02 add

--- atv1p.py
+++
@@ -1,9 +0,0 @@
-
-Questão 1
-# Recebe o valor de n1, número inteiro que vai ser checado se é par.
-n1 = int(input('Digite um número maior que 0: '))
-# checa, se n1 for divisivel por 2 e o resto for 0, o número é par, caso contrário, não é par.
-if n1 % 2 == 0:
-    print(f'O seu número {n1} é par!')
-else:
-    print(f'O seu número {n1} não é par :/')
--- atv4.py
+++
@@ -1,8 +0,0 @@
-n1 = int(input('Digite o primeiro número: '))
-n2 = int(input('Digite o segundo número: '))
-n3 = int(input('Digite o terceiro número: '))
-
-if n1 == n2 and n2 == n3:
-    print(f'Seus números, {n1},{n2},{n3} são todos iguais!')
-else:
-    print('Seus números não são iguais :/')    
